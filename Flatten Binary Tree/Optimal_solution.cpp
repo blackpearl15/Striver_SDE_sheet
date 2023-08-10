34ee77c0ae1f545dcad88dc8a5ce1ab55fb2535a
blackpearl15 <yashanand2477@gmail.com>
2023-08-11 01:08:25 +0530

    Optimal solution

---
+++ Flatten Binary Tree/Optimal_solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure.
+
+    template <typename T>
+    class TreeNode {
+        public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+// Bascially traced the logic and learnt it
+// To revise concept trace it again
+
+void flatten(TreeNode<int> *root,TreeNode<int> *&prev){
+    if(root==NULL) return;
+
+    flatten(root->right,prev);
+    flatten(root->left,prev);
+
+    root->right=prev;
+    root->left=NULL;
+    prev = root;
+}
+
+TreeNode<int> *flattenBinaryTree(TreeNode<int> *root)
+{
+    // Write your code here.
+    TreeNode<int> *prev = NULL;
+    flatten(root,prev);
+
+    return root;
+}
